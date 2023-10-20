4e79311dfb829c109a36378c28d3c29a86de5f02
ParasPandey019 <paraspandey019@gmail.com>
2023-10-20 22:21:55 +0530

    necessary files added

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect('mongodb://127.0.0.1:27017/pollingAPI');
+// mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console, 'error connecting to db'));
+db.once('open', function(){
+    console.log('successfully connected to databse');
+})
+
+module.exports = db;
